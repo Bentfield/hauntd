fd677b31156d88db8fd13ac56563e97025201f4b
Dylan Bentfield <dylangb2@illinois.edu>
2019-10-25 13:41:41 -0500

    Add Airbnb styling

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import Buefy from 'buefy'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import Buefy from 'buefy';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(Buefy)
+Vue.use(Buefy);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
